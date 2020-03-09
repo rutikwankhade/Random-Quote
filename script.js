cb5d0554ccc9d9cacaa951ef32b49a340bccb53f
rutik wankhade <rutikwankhade2@gmail.com>
2020-03-09 18:53:00 +0530

    added more styling

--- script.js
+++ script.js
@@ -34,13 +34,14 @@ let btn=document.querySelector('.btn');
 btn.addEventListener('click', displayQuote);
 
 function displayQuote(){
-    //create an index of arandom number 
-    //convert it into between 1 to 25
+    //create an index of a random number 
+    //convert it into between 0 to 24
     let index=Math.floor(Math.random()*24);
    
     //display the quote of that index
     let div=document.querySelector('#quote');
     let quote=`<div class="card">
+    <img src="icons/favicon.png">
     <p>${quotes[index]}</p>
     <img src="icons/favicon.png">
     </div>`;
