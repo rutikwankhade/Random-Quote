e202f21d39212b6d32ba83e2bf3c2e1c5a30afe3
Rutik Wankhade <rutikwankhade2@gmail.com>
2020-03-25 13:07:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ btn.addEventListener('click', displayQuote);
 function displayQuote(){
     //create an index of a random number 
     //convert it into between 0 to 24
-    let index=Math.floor(Math.random()*24);
+    let index=Math.floor(Math.random()*quotes.length);
    
     //display the quote of that index
     let div=document.querySelector('#quote');
